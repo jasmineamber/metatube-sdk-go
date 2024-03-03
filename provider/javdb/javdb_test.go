ad5c32e3caedd2f7eabce044e1c297281fa98e65
jasmineamber <jasmineamber@aliyun.com>
2024-03-03 19:14:07 +0800

    Add Provider: JAVDB

--- provider/javdb/javdb_test.go
+++ provider/javdb/javdb_test.go
@@ -1,43 +1,22 @@
 package javdb
 
 import (
-	"encoding/json"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/metatube-community/metatube-sdk-go/provider/internal/testkit"
 )
 
 func TestJavDB_GetMovieInfoByID(t *testing.T) {
-	provider := New()
-	for _, item := range []string{
+	testkit.Test(t, New, []string{
 		"Yw8XB",
-		//"BabyGotBoobs.18.12.27",
-		//"BigNaturals.BigNaturals.23.05.27 Big Tits VR",
-		//"BigNaturals Big Tits VR",
-		//"BabyGotBoobs Lovely In Latex",
-	} {
-		info, err := provider.GetMovieInfoByID(item)
-		data, _ := json.MarshalIndent(info, "", "\t")
-		assert.True(t, assert.NoError(t, err) && assert.True(t, info.Valid()))
-		t.Logf("%s", data)
-	}
+	})
 }
 
 func TestJavBus_SearchMovie(t *testing.T) {
-	//"http://127.0.0.1:8080/v1/movies/search?q=BigNaturals+Big+Tits+VR&provider=&fallback=True"
-	provider := New()
-	for _, item := range []string{
-		// "BigNaturals Big Tits VR",
-		// "BabyGotBoobs Lovely In Latex",
+	testkit.Test(t, New, []string{
 		"ARM-383",
-	} {
-		results, err := provider.SearchMovie(provider.NormalizeMovieKeyword(item))
-		data, _ := json.MarshalIndent(results, "", "\t")
-		if assert.NoError(t, err) {
-			for _, result := range results {
-				assert.True(t, result.Valid())
-			}
-		}
-		t.Logf("%s", data)
-	}
+		"MEYD-856",
+		"SSIS-354",
+		"HMN",
+	})
 }
